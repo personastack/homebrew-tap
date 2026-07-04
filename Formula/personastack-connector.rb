658078e5871249cab075c796783fd6cf9d27a2de
Eric Greer <integrii@gmail.com>
2026-07-04 01:48:27 -0700

    feat: update connector formula v0.7.2

--- Formula/personastack-connector.rb
+++ Formula/personastack-connector.rb
@@ -1,26 +1,26 @@
 class PersonastackConnector < Formula
   desc "Local Connector for PersonaStack external personas"
   homepage "https://personastack.ai"
-  version "0.6.0"
+  version "0.7.2"
   license :cannot_represent
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/personastack/personastack-connector/releases/download/v0.6.0/personastack-connector_0.6.0_darwin_arm64.tar.gz"
-      sha256 "a54aad894d4032e893fd6c44a09bea2426e79e83d0f8575fe9b236375edc7bcc"
+      url "https://github.com/personastack/personastack-connector/releases/download/v0.7.2/personastack-connector_0.7.2_darwin_arm64.tar.gz"
+      sha256 "2ca21a36f055da22b1b002da9130324132b4b228199c224eb5fe24ed768b91ac"
     else
-      url "https://github.com/personastack/personastack-connector/releases/download/v0.6.0/personastack-connector_0.6.0_darwin_amd64.tar.gz"
-      sha256 "40e8354b5ee1085198113c5db6d44d802584b15ac8311eca8cad5375d0b2549f"
+      url "https://github.com/personastack/personastack-connector/releases/download/v0.7.2/personastack-connector_0.7.2_darwin_amd64.tar.gz"
+      sha256 "9170763626dc111069c415cac22ac4d71510cf526f7dfdad2076770ae4eaaf2c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/personastack/personastack-connector/releases/download/v0.6.0/personastack-connector_0.6.0_linux_arm64.tar.gz"
-      sha256 "5df781de07a6e00af56c978766398aba1d8005253e58fd4b08f8fd3fb89d19ea"
+      url "https://github.com/personastack/personastack-connector/releases/download/v0.7.2/personastack-connector_0.7.2_linux_arm64.tar.gz"
+      sha256 "02d2779d57500b28374e04eaebbea53579312e518b5beb934c43a014226dadb9"
     else
-      url "https://github.com/personastack/personastack-connector/releases/download/v0.6.0/personastack-connector_0.6.0_linux_amd64.tar.gz"
-      sha256 "e80ee531b7b63c17ae985b4f1da6f5171cf5233501576aa0e6618875872fe2ad"
+      url "https://github.com/personastack/personastack-connector/releases/download/v0.7.2/personastack-connector_0.7.2_linux_amd64.tar.gz"
+      sha256 "7d1092be0b45feb0129e1d13f92cb7287f99dcf7ce367bd472f63fc1ff5f13b4"
     end
   end
 
